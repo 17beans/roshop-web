48a870d1fde84485339a5c023b2fa11a3196383f
17kbeans <17beans@naver.com>
2023-12-29 21:49:04 +0900

    Adding OAuth Login

--- src/app/SignIn/page.tsx
+++ src/app/SignIn/page.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import Stack from '@mui/material/Stack';
 import { signIn, useSession } from "next-auth/react";
 import { DiscordLoginButton, GithubLoginButton, GoogleLoginButton } from "react-social-login-buttons";
+import { FaGithub, FaDiscord, FaGoogle } from 'react-icons/fa'
 
 
 
@@ -17,28 +18,37 @@ const GetPlatformNameCamelCase = (PlatformName: string) => {
 }
 
 const SignInButton = ({ PlatformName, buttonColor, textColor = "text-white" }: { PlatformName: string, buttonColor: string, textColor: string }) => {
-    return (
-        <div>
+    const SignInButton = ({ PlatformName }: { PlatformName: string }) => {
+        const bgColor = PlatformName == 'github' && 'bg-github' || PlatformName == 'discord' && 'bg-discord' || PlatformName == 'google' && 'bg-google' || PlatformName == 'naver' && 'bg-naver' || PlatformName == 'kakao' && 'bg-kakao';
+        const textColor = PlatformName == 'github' && 'text-github' || PlatformName == 'discord' && 'text-discord' || PlatformName == 'google' && 'text-google' || PlatformName == 'naver' && 'text-naver' || PlatformName == 'kakao' && 'text-kakao';
+        const icon = PlatformName == 'github' && FaGithub || PlatformName == 'discord' && FaDiscord || PlatformName == FaGoogle && 'text-google' || PlatformName == 'naver' && (() => { }) || PlatformName == 'kakao' && (() => { })
+
+        return (
             <button
-                className={`mt-4 ${buttonColor} ${textColor} px-6 py-3 rounded-lg`}
+                className={`flex mt-4 ${bgColor} ${textColor} px-6 py-3 rounded-lg`}
                 onClick={() => signIn(PlatformName)}
             >
+                {icon()}
                 Sign in with {GetPlatformNameCamelCase(PlatformName)}
             </button>
-        </div>
-    )
-}
+        )
+    }
 
 
 
-export default function SignIn({ }: {}) {
-    const { data: session, status } = useSession();
+    export default function SignIn({ }: {}) {
+        const { data: session, status } = useSession();
 
-    return (
-        <div className={`text-center w-full mx-auto max-w[1240px] mt-16`}>
-            <GithubLoginButton onClick={() => signIn("github")} className={`mx-auto mt-16`} />
-            <DiscordLoginButton onClick={() => signIn("discord")} className={`mx-auto mt-16`} />
-            <GoogleLoginButton onClick={() => signIn("google")} className={`mx-auto mt-16`} />
-        </div>
-    )
-}
+        return (
+            <div className={`flex-col`}>
+                <GithubLoginButton onClick={() => signIn("github")} className={`mx-auto mt-16`} />
+                <DiscordLoginButton onClick={() => signIn("discord")} className={`mx-auto mt-16`} />
+                <GoogleLoginButton onClick={() => signIn("google")} className={`mx-auto mt-16`} />
+                <SignInButton PlatformName="github" />
+                <SignInButton PlatformName="discord" />
+                <SignInButton PlatformName="google" />
+                <SignInButton PlatformName="naver" />
+                <SignInButton PlatformName="kakao" />
+            </div>
+        )
+    }
